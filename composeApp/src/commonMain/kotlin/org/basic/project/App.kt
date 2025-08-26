f4900f0dc280477e57d3deefd95b3d2fbcbb6467
almaeq <alma.quinterosquiroga@gmail.com>
2025-08-26 19:30:51 -0300

    third screen

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -3,6 +3,7 @@ package org.basic.project
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,8 +18,13 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -28,6 +34,17 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        Navigator(
+            screen = MainScreen()
+        )
+    }
+}
+
+class MainScreen: Screen{
+
+    @Composable
+    override fun Content() {
+        val navigator: Navigator = LocalNavigator.currentOrThrow
         var name: String by remember { mutableStateOf(value = "") }
         var pass: String by remember { mutableStateOf(value = "") }
         Column (
@@ -66,6 +83,103 @@ fun App() {
                 Text(
                     text = "Contraseña $pass",
                     fontSize = 20.sp
+                )
+            }
+
+            Spacer (modifier = Modifier.height(40.dp))
+
+            Button(
+                onClick = {
+                    navigator.push(SecondScreen())
+                }
+            ) {
+                Text(
+                    text = "second screen"
+                )
+            }
+
+            Spacer (modifier = Modifier.height(40.dp))
+
+            Button(
+                onClick = {
+                    navigator.push(ThirdScreen())
+                }
+            ) {
+                Text(
+                    text = "third screen"
+                )
+            }
+        }
+
+    }
+}
+
+class SecondScreen: Screen {
+
+    @Composable
+    override fun Content() {
+        val navigator: Navigator=LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Yellow),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Segunda pantalla",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.replaceAll(MainScreen())
+                }
+            ) {
+                Text(
+                    text = "Vamos para atrás"
+                )
+            }
+        }
+    }
+
+}
+
+class ThirdScreen: Screen{
+    @Composable
+    override fun Content() {
+        val navigator: Navigator=LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Magenta),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Tercera pantalla",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+            )
+
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text(
+                    text = "Vamos a la 1"
                 )
             }
         }
